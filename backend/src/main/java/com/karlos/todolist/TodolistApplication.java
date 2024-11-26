bad1e9031ed79e336a3e358c5fef4c438498a866
João Pedro <joaoptgabriel96@gmail.com>
2024-11-26 19:13:50 -0300

    Update TodolistApplication.java

--- backend/src/main/java/com/karlos/todolist/TodolistApplication.java
+++ backend/src/main/java/com/karlos/todolist/TodolistApplication.java
@@ -3,9 +3,11 @@ package com.karlos.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.Generated;
+
 @SpringBootApplication
 public class TodolistApplication {
-
+	@Generated("ignore-coverage")
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
