e15494844b102bc0cbc0ab3b08a1f9f5a34f9041
João Pedro <joaoptgabriel96@gmail.com>
2024-11-26 19:19:39 -0300

    Update to ignore uncovered main

--- backend/src/main/java/com/karlos/todolist/TodolistApplication.java
+++ backend/src/main/java/com/karlos/todolist/TodolistApplication.java
@@ -3,11 +3,9 @@ package com.karlos.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.Generated;
 
 @SpringBootApplication
 public class TodolistApplication {
-	@Generated("ignore-coverage")
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
