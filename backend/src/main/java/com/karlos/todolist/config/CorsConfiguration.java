1c8e0c029d2f590629e9cc6b4299f957bf0f70fc
João Pedro <joaoptgabriel96@gmail.com>
2024-11-26 19:38:08 -0300

    Update CorsConfiguration.java

--- backend/src/main/java/com/karlos/todolist/config/CorsConfiguration.java
+++ backend/src/main/java/com/karlos/todolist/config/CorsConfiguration.java
@@ -4,13 +4,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CorsConfiguration implements WebMvcConfigurer { // NOSONAR
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    } // NOSONAR
+        registry.addMapping("/**") // NOSONAR
+                .allowedOrigins("*") // NOSONAR
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") // NOSONAR
+                .allowedHeaders("*") // NOSONAR
+                .allowCredentials(true); // NOSONAR
+    }
 }
