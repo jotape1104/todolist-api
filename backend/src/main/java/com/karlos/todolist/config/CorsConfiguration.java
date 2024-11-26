f04d3051fba735fbff96fe636d8e48bbd4e95020
Karlos Eduardo Kaminski <kaminski0708@hotmail.com>
2024-11-26 19:33:12 -0300

    Update CorsConfiguration.java

--- backend/src/main/java/com/karlos/todolist/config/CorsConfiguration.java
+++ backend/src/main/java/com/karlos/todolist/config/CorsConfiguration.java
@@ -3,14 +3,14 @@ package com.karlos.todolist.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration implements WebMvcConfigurer { // NOSONAR
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+    @Override // NOSONAR
+    public void addCorsMappings(CorsRegistry registry) { // NOSONAR
+        registry.addMapping("/**") // NOSONAR
+                .allowedOrigins("*") // NOSONAR
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") // NOSONAR
+                .allowedHeaders("*") // NOSONAR
+                .allowCredentials(true); // NOSONAR
+    } // NOSONAR
 }
