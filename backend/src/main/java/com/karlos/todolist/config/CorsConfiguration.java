af911ae5cf2f266c52827b34b528d8fe0ec9e6df
João Pedro <joaoptgabriel96@gmail.com>
2024-12-02 21:34:13 -0300

    Update CorsConfiguration.java

--- backend/src/main/java/com/karlos/todolist/config/CorsConfiguration.java
+++ backend/src/main/java/com/karlos/todolist/config/CorsConfiguration.java
@@ -3,15 +3,13 @@ package com.karlos.todolist.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfiguration implements WebMvcConfigurer { // sonar-ignore
+public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // sonar-ignore-begin
         registry.addMapping("/**") 
                 .allowedOrigins("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") 
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
     }
-    // sonar-ignore-end
 }
